f8f6156da544ba1d43cdd0dec684162514edb17e
Ayrat Fatykhov <airat_91_91@mail.ru>
2024-04-29 21:00:20 +0300

    add register User

--- users/views.py
+++ users/views.py
@@ -1,18 +1,17 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.permissions import AllowAny
 
-from users.models import Payment
-from users.serializers import PaymentSerializer
+from users.models import Payment, User
+from users.serializers import PaymentSerializer, UserSerializer
 
 
 class PaymentCreateAPIView(CreateAPIView):
-
     serializer_class = PaymentSerializer
 
 
 class PaymentListAPIView(ListAPIView):
-
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
     filter_backends = [DjangoFilterBackend, OrderingFilter]
@@ -21,17 +20,25 @@ class PaymentListAPIView(ListAPIView):
 
 
 class PaymentRetrieveAPIView(RetrieveAPIView):
-
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
 
 
 class PaymentUpdateAPIView(UpdateAPIView):
-
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
 
 
 class PaymentDestroyAPIView(DestroyAPIView):
-
     queryset = Payment.objects.all()
+
+
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
