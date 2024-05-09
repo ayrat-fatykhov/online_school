6ade925aa6b7a311318821e31d8107324784cad3
Ayrat Fatykhov <airat_91_91@mail.ru>
2024-05-09 12:24:55 +0300

    add validator for lesson.url

---
+++ materials/validators.py
@@ -0,0 +1,13 @@
+from rest_framework.serializers import ValidationError
+
+
+class UrlValidator:
+
+    def __init__(self, url):
+        self.url = url
+
+    def __call__(self, value):
+        temp_val = dict(value).get(self.url)
+        if temp_val:
+            if "youtube.com" not in temp_val:
+                raise ValidationError('URL is forbidden')
