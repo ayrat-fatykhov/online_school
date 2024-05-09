ab4b287c484df33f2405150f9405844a889d6f4c
Ayrat Fatykhov <airat_91_91@mail.ru>
2024-05-09 12:17:16 +0300

    add urls for subscription

--- materials/urls.py
+++ materials/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 from materials.apps import MaterialsConfig
 from materials.views import CourseViewSet, LessonCreateView, LessonListView, LessonRetrieveView, LessonUpdateView, \
-    LessonDestroyView
+    LessonDestroyView, SubscriptionListAPIView, SubscriptionCreateAPIView, SubscriptionDestroyAPIView
 
 app_name = MaterialsConfig.name
 router = DefaultRouter()
@@ -15,4 +15,7 @@ urlpatterns = [
     path('lesson/view/<int:pk>/', LessonRetrieveView.as_view(), name='lesson_view'),
     path('lesson/update/<int:pk>/', LessonUpdateView.as_view(), name='lesson_update'),
     path('lesson/delete/<int:pk>/', LessonDestroyView.as_view(), name='lesson_delete'),
+    path('subscriptions/', SubscriptionListAPIView.as_view(), name='subs_list'),
+    path('subscriptions/create/', SubscriptionCreateAPIView.as_view(), name='subs_create'),
+    path('subscriptions/delete/<int:pk>/', SubscriptionDestroyAPIView.as_view(), name='subs_delete'),
 ] + router.urls
